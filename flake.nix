94a5f4073a91d4eb75128000cb7bff0d836ec898
Gregory C. Oakes <gregcoakes@gmail.com>
2021-04-14 20:15:21 -0500

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
+        with pkgs; {
+          # `nix develop`
+          devShell = mkShell {
+            nativeBuildInputs = [ rustc cargo ];
+          };
+        }
+    );
+}
