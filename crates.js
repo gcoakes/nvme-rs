88452db18af1498c3ce186e75930f693cf6b6c79
gcoakes <gregcoakes@gmail.com>
2021-04-16 06:20:32 +0000

    Deploying to gh-pages from @ gcoakes/nvme-rs@137b37ca23d68a252111dfa43cc5bac5f185888c 🚀

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["modular_bitfield","modular_bitfield_impl","num_derive","num_traits","nvme","proc_macro2","quote","static_assertions","syn","unicode_xid"];
+window.ALL_CRATES = ["modular_bitfield","modular_bitfield_impl","num_derive","num_traits","nvme","proc_macro2","quote","serde","serde_derive","static_assertions","syn","unicode_xid"];
